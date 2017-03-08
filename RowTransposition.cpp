343dc72da7a798dfb49f3afe617372b7de9890c9
mrnguyen <you@example.com>
2017-03-07 18:27:26 -0800

    almost done RowT

--- RowTransposition.cpp
+++ RowTransposition.cpp
@@ -56,7 +56,6 @@ string RowTransposition::encrypt(const string& plainText)
 
 	while (encryptedText.length() < plainText.length() )
 	{
-		//if (columnIndex < extraLetterColumns)
 		if ( (vect.at(vectorIndex) - 1) < extraLetterColumns)
 		{
 			for (int x = 0; x < columnLength+1; x++)
@@ -97,6 +96,33 @@ string RowTransposition::encrypt(const string& plainText)
 string RowTransposition::decrypt(const string& cipherText) 
 { 
 	string decryptedText ="";
+
+	int stringLength = cipherText.length();
+	int columnAmount = vect.size();
+	int columnLength = stringLength / columnAmount;
+	int extraLetterColumns = stringLength % columnAmount;
+	int index = 0;
+	//int rowIndex = 0;
+	//int columnIndex = 0; 
+	int vectorIndex = 0;
+	cout << stringLength<< " "<< columnAmount <<" " << columnLength<<" "<< extraLetterColumns<< endl;
+	
+	while (decryptedText.length() < cipherText.length() )
+	{
+		for (int y = 0; y < columnLength; y++)
+		{
+			for (int x = 0; x < columnAmount; x++)
+			{
+				index = columnLength *(vect.at(vectorIndex) - 1 ) + y ;
+				decryptedText = decryptedText + cipherText[index];
+				cout<< index <<" ";
+				//rowIndex++;
+				vectorIndex++;
+			}
+			vectorIndex = 0;
+		}
+	}
+	
 
 	return decryptedText; 
 }
